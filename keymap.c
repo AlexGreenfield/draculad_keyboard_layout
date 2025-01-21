1078ddfe1d02be630ef2894df7d8e3b1f38ddb81
AlexGreenfield <alejandrocastroverde@gmail.com>
2025-01-21 13:25:31 +0100

    fixed some bugs

--- keymap.c
+++ keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LT(_MUS,KC_A),         LGUI_T(KC_S),    LALT_T(KC_D),    LCTL_T(KC_F),    KC_G,                         KC_H,             RCTL_T(KC_J),    LALT_T(KC_K),    RGUI_T(KC_L),    RALT_T(KC_SCLN),
         LSFT_T(KC_Z), KC_X,    KC_C,    KC_V,    KC_B,                                                 KC_N,             KC_M,    KC_COMM, KC_DOT,  RSFT_T(KC_SLSH),
                                                  TG(_GAME),                                              TG(_ADJ),
-                                        KC_ESC, KC_SPC, KC_TAB,    LT(_NUM,KC_ENT), LT(_SYMB,KC_BSPC), KC_DEL
+                                        KC_ESC, LT(_NUM,KC_SPC), KC_TAB,    KC_ENT, LT(_SYMB,KC_BSPC), KC_DEL
     ),
     [_NUM] = LAYOUT(
         KC_F1,   KC_F2,    KC_F3,    KC_F4,    KC_F5,                         KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,
@@ -64,8 +64,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_GAME] =  LAYOUT(
         KC_TAB,         KC_1,    KC_2,    KC_3,    KC_4,                                                 KC_Y,             KC_U,    KC_I,    KC_O,    KC_P,
-        KC_LSFT,        KC_E,    KC_W,    KC_R,    KC_T,                                                 KC_Y,             KC_U,    KC_I,    KC_O,    KC_P,
-        KC_LCTL,        KC_A,    KC_S,    KC_D,    KC_F,                         KC_H,             RCTL_T(KC_J),    LALT_T(KC_K),    RGUI_T(KC_L),    RALT_T(KC_SCLN),
+        KC_LCTL,        KC_Q,    KC_W,    KC_E,    KC_R,                                                 KC_Y,             KC_U,    KC_I,    KC_O,    KC_P,
+        KC_LSFT,        KC_A,    KC_S,    KC_D,    KC_F,                         KC_H,             RCTL_T(KC_J),    LALT_T(KC_K),    RGUI_T(KC_L),    RALT_T(KC_SCLN),
                                                  TG(_GAME),                                              TG(_ADJ),
                                         KC_B, KC_SPC, KC_SPC,    LT(_NUM,KC_ENT), LT(_SYMB,KC_BSPC), KC_DEL
     ),
@@ -188,6 +188,9 @@ static void render_status(void) {
             break;
         case _MUS:
             oled_write_P(PSTR("Mouse  "), false);
+            break;
+        case _GAME:
+            oled_write_P(PSTR("Game  "), false);
             break;
         default:
             oled_write_P(PSTR("Unkn "), false);
